6a3b16761b9cb80bb5381e50b8311a3c64221182
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-26 15:20:27 -0300

    refactor(lsp): migrate to Neovim 0.11+ built-in LSP config
    - Return config tables from lsp/*.lua instead of using vim.lsp.config()
    - Delete empty LSP config files (bashls, clangd, cmake, dockerls, etc.)
    - Replace deprecated vim.lsp.with() with vim.lsp.set_handler()
    - Simplify hover handler signature

--- lua/fruet/lsp.lua
+++ lua/fruet/lsp.lua
@@ -3,28 +3,20 @@ vim.lsp.enable('ruff', false)
 
 -- This strips out &nbsp; and some ending escaped backslashes out of hover
 -- strings because the pyright LSP is... odd with how it creates hover strings.
-local hover = function(_, result, ctx, config)
+local hover = function(result, ctx)
     if not (result and result.contents) then
-        return vim.lsp.handlers.hover(_, result, ctx, config)
+        return
     end
     if type(result.contents) == "string" then
         local s = string.gsub(result.contents or "", "&nbsp;", " ")
         s = string.gsub(s, [[\\\n]], [[\n]])
         result.contents = s
-        return vim.lsp.handlers.hover(_, result, ctx, config)
     else
         local s = string.gsub((result.contents or {}).value or "", "&nbsp;", " ")
         s = string.gsub(s, "\\\n", "\n")
         result.contents.value = s
-        return vim.lsp.handlers.hover(_, result, ctx, config)
     end
+    return vim.lsp.handlers.hover(result, ctx)
 end
 
--- rest of lsp config goes here
--- this get passed into lspconfig.setup
---  or server:setup_lsp() from nvim-lsp-installer
-local lsp_setup_config = {
-    handlers = {
-        ["textDocument/hover"] = vim.lsp.with(hover),
-    },
-}
+vim.lsp.set_handler("hover", hover)
