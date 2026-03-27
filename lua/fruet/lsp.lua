157a42a41f227e3c6d275fd363231373741ad028
gabrielfruet <gabrielfruet538@gmail.com>
2026-03-27 14:07:37 -0300

    removed gitsigns

--- lua/fruet/lsp.lua
+++ lua/fruet/lsp.lua
@@ -3,20 +3,20 @@ vim.lsp.enable('ruff', false)
 
 -- This strips out &nbsp; and some ending escaped backslashes out of hover
 -- strings because the pyright LSP is... odd with how it creates hover strings.
-local hover = function(result, ctx)
-    if not (result and result.contents) then
-        return
-    end
-    if type(result.contents) == "string" then
-        local s = string.gsub(result.contents or "", "&nbsp;", " ")
-        s = string.gsub(s, [[\\\n]], [[\n]])
-        result.contents = s
-    else
-        local s = string.gsub((result.contents or {}).value or "", "&nbsp;", " ")
-        s = string.gsub(s, "\\\n", "\n")
-        result.contents.value = s
-    end
-    return vim.lsp.handlers.hover(result, ctx)
-end
-
-vim.lsp.set_handler("hover", hover)
+-- local hover = function(result, ctx)
+--     if not (result and result.contents) then
+--         return
+--     end
+--     if type(result.contents) == "string" then
+--         local s = string.gsub(result.contents or "", "&nbsp;", " ")
+--         s = string.gsub(s, [[\\\n]], [[\n]])
+--         result.contents = s
+--     else
+--         local s = string.gsub((result.contents or {}).value or "", "&nbsp;", " ")
+--         s = string.gsub(s, "\\\n", "\n")
+--         result.contents.value = s
+--     end
+--     return vim.lsp.handlers.hover(result, ctx)
+-- end
+--
+-- vim.lsp.set_handler("hover", hover)
